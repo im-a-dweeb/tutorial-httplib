bd2ec2a25c2a253dbb506114cc1d4bf55ee7da18
Lufthor <127716957+im-a-dweeb@users.noreply.github.com>
2024-07-29 17:03:24 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,21 +1,2 @@
 #!/bin/bash
 g++ -L/home/anthony/coding/vcpkg/installed/x64-linux/lib -I/home/anthony/coding/vcpkg/installed/x64-linux/include webserver.cpp -o webserver
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                            
